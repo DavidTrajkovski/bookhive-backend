5a76f99f3a46ceab583343a53d6c311152835337
dev-marko <markospasenovski00@gmail.com>
2023-08-09 23:49:26 +0200

    Add PhoneNumber prop to BookShopDto

--- BookHiveDB.Domain/Dtos/Rest/BookShop/BookShopDto.cs
+++ BookHiveDB.Domain/Dtos/Rest/BookShop/BookShopDto.cs
@@ -9,6 +9,7 @@ public class BookShopDto : BaseEntity
     public string City { get; set; }
     public string Address { get; set; }
     public string Email { get; set; }
+    public string PhoneNumber { get; set; }
     public string WebsiteLink { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
