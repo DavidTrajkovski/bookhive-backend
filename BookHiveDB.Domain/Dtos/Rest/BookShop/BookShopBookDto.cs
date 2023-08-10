e9eda5cc1659af4aa6909b9ada24c2f6d5198738
dev-marko <markospasenovski00@gmail.com>
2023-08-10 22:56:15 +0200

    Create BookShopBookDto

---
+++ BookHiveDB.Domain/Dtos/Rest/BookShop/BookShopBookDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BookHiveDB.Domain.Dtos.REST.Book;
+
+namespace BookHiveDB.Domain.Dtos.Rest.BookShop;
+
+public class BookShopBookDto : BaseEntity
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public List<BookAuthorInfoDto> Authors { get; set; }
+}
