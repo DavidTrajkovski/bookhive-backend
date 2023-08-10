bdf75a4276e6823a82d3e907ec02e01db4014f66
dev-marko <markospasenovski00@gmail.com>
2023-08-10 22:56:08 +0200

    Add BookshopEmail property to BookShopDto

--- BookHiveDB.Domain/Dtos/Rest/BookShop/BookShopDto.cs
+++ BookHiveDB.Domain/Dtos/Rest/BookShop/BookShopDto.cs
@@ -8,7 +8,7 @@ public class BookShopDto : BaseEntity
     public string Name { get; set; }
     public string City { get; set; }
     public string Address { get; set; }
-    public string Email { get; set; }
+    public string BookshopEmail { get; set; }
     public string PhoneNumber { get; set; }
     public string WebsiteLink { get; set; }
     public double Latitude { get; set; }
