97d6f81303d2dc007332986a28f0f347ac1a06d0
dev-marko <markospasenovski00@gmail.com>
2023-08-11 22:19:28 +0200

    Create initial BookClubDto

---
+++ BookHiveDB.Domain/Dtos/Rest/BookClub/BookClubDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookHiveDB.Domain.Dtos.Rest.BookClub;
+
+public class BookClubDto : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Owner { get; set; }
+}
