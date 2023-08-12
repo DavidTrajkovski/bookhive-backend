539d25bd077762570a588e068b4ee0e0a663809d
blagojpetkov <85490029+blagojpetkov@users.noreply.github.com>
2023-08-12 15:55:36 +0200

    Add edit profile logic

---
+++ BookHiveDB.Domain/Dtos/Rest/UpdateUserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookHiveDB.Domain.Dtos.Rest
+{
+    public class UpdateUserDTO
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+    }
+}
---
+++ BookHiveDB.Web/Controllers/Api/AccountApiController.cs
@@ -0,0 +1,64 @@
+﻿using BookHiveDB.Domain.Dtos.Rest;
+using BookHiveDB.Domain.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BookHiveDB.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountApiController : ControllerBase
+    {
+
+        private readonly UserManager<BookHiveApplicationUser> _userManager;
+        private readonly SignInManager<BookHiveApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountApiController(UserManager<BookHiveApplicationUser> userManager, SignInManager<BookHiveApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        [HttpPost("edit")]
+        //[Authorize("Default")]
+        public IActionResult EditProfile([FromBody] UpdateUserDTO userDTO)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = _userManager.FindByIdAsync(userId).Result;
+
+
+
+            if (userDTO.password != null && !userDTO.password.Equals(userDTO.confirmPassword))
+            {
+                return Unauthorized(new { message = "Passwords do not match." });
+            }
+
+            user.FirstName = userDTO.firstName;
+            user.LastName = userDTO.lastName;
+            user.Address = userDTO.address;
+            if (userDTO.password == null)
+            {
+
+                var updateResult = _userManager.UpdateAsync(user).Result;
+                return Ok();
+            }
+
+
+
+            var token = _userManager.GeneratePasswordResetTokenAsync(user).Result;
+            var result = _userManager.ResetPasswordAsync(user, token, userDTO.password).Result;
+            if (!result.Succeeded)
+            {
+                return Ok();
+            }
+
+            var updateResultWithPasswordChange = _userManager.UpdateAsync(user).Result;
+
+            return Ok();
+        }
+    }
+}
