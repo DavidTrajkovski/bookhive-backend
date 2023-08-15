d481965e0489ad0b41825fa2f83aeb565d28e743
dev-marko <markospasenovski00@gmail.com>
2023-08-15 23:22:01 +0200

    Add CreateBookClubDto

---
+++ BookHiveDB.Domain/Dtos/Rest/BookClub/CreateBookClubDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookHiveDB.Domain.Dtos.Rest.BookClub;
+
+public class CreateBookClubDto
+{
+    public string OwnerId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
