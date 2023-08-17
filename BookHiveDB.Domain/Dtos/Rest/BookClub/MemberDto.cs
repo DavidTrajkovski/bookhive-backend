383b8c0613095ff8ea112a9625b9f2d4a6f2315a
dev-marko <markospasenovski00@gmail.com>
2023-08-17 23:31:31 +0200

    Create MembersDto

---
+++ BookHiveDB.Domain/Dtos/Rest/BookClub/MemberDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookHiveDB.Domain.Dtos.Rest.BookClub;
+
+public class MemberDto : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
