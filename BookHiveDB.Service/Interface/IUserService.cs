5809d33a0c62916fb4e4501357c24ed78f646e65
dev-marko <markospasenovski00@gmail.com>
2023-08-20 23:49:43 +0200

    Implement BookHiveUserEmails method

--- BookHiveDB.Service/Interface/IUserService.cs
+++ BookHiveDB.Service/Interface/IUserService.cs
@@ -1,9 +1,11 @@
-﻿using BookHiveDB.Domain.Identity;
+﻿using System.Collections.Generic;
+using BookHiveDB.Domain.Identity;
 
 namespace BookHiveDB.Service.Interface
 {
     public interface IUserService
     {
+        IEnumerable<string> BookHiveUserEmails();
         BookHiveApplicationUser findById(string id);
         BookHiveApplicationUser findByEmail(string email);
         BookHiveApplicationUser update(BookHiveApplicationUser user, string name, string surname, string email, string password, string confirmPassword);
