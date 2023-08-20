5faed4dccc16bada02032465e80ba0a5dd2f328d
dev-marko <markospasenovski00@gmail.com>
2023-08-20 20:33:22 +0200

    Add new Invitation DTOs

---
+++ BookHiveDB.Domain/Dtos/Rest/BookClub/InvitationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookHiveDB.Domain.Dtos.Rest.BookClub;
+
+public class InvitationDto : BaseEntity
+{
+    public string BookClubName { get; set; }
+    public string SenderEmail { get; set; }
+    public string ReceiverId { get; set; }
+    public string Message { get; set; }
+    public bool IsRequest { get; set; }
+}
