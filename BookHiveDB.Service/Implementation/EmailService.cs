212a6d88827691e7e56038004d8361300f3a5de5
dev-marko <markospasenovski00@gmail.com>
2023-09-04 00:32:00 +0200

    Update MailKit and reactivate email service

--- BookHiveDB.Service/Implementation/EmailService.cs
+++ BookHiveDB.Service/Implementation/EmailService.cs
@@ -66,5 +66,41 @@ namespace BookHiveDB.Service.Implementation
                 throw ex;
             }
         }
+
+        public async Task SendEmailAsync(EmailMessage mail)
+        {
+            var emailMessage = new MimeMessage
+            {
+                Sender = new MailboxAddress(_settings.SendersName, _settings.SmtpUserName),
+                Subject = mail.Subject
+            };
+
+            emailMessage.From.Add(new MailboxAddress(_settings.EmailDisplayName, _settings.SmtpUserName));
+
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Plain) { Text = mail.Content };
+
+            emailMessage.To.Add(new MailboxAddress(mail.MailTo, mail.MailTo));
+
+            try
+            {
+                using var smtp = new MailKit.Net.Smtp.SmtpClient();
+                var socketOptions = _settings.EnableSsl ? SecureSocketOptions.StartTls : SecureSocketOptions.Auto;
+
+                await smtp.ConnectAsync(_settings.SmtpServer, _settings.SmtpServerPort, socketOptions);
+
+                if (!string.IsNullOrEmpty(_settings.SmtpUserName))
+                {
+                    await smtp.AuthenticateAsync(_settings.SmtpUserName, _settings.SmtpPassword);
+                }
+
+                await smtp.SendAsync(emailMessage);
+
+                await smtp.DisconnectAsync(true);
+            }
+            catch (SmtpException ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
--- BookHiveDB.Service/Interface/IEmailService.cs
+++ BookHiveDB.Service/Interface/IEmailService.cs
@@ -7,5 +7,6 @@ namespace BookHiveDB.Service.Interface
     public interface IEmailService
     {
         Task SendEmailAsync(List<EmailMessage> allMails);
+        Task SendEmailAsync(EmailMessage mail);
     }
 }
